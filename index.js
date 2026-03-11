b00e075b576f53af5a1b834adc781753eae15f3b
ringgooo <rjaferrer29@src.pshs.edu.ph>
2026-03-11 15:59:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function validateForm() {
-    let text = document.forms["grocery-form"]["text"].value;
-    document.forms["grocery-form"]["text"].value = text.trim();
+    let text = document.forms["grocery-form"]["customer"].value;
+    document.forms["grocery-form"]["customer"].value = text.trim();
     if (text.trim() === ""){
         alert("Enter your name!");
         return false;
